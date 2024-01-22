9c3e8a39ced741729a5c544691e08b69d8a8d920
wminsoo1 <wminsoo1@naver.com>
2024-01-22 23:30:35 +0900

    아무렇게나

--- app/src/main/java/com/example/burn/MainActivity.kt
+++ app/src/main/java/com/example/burn/MainActivity.kt
@@ -9,6 +9,7 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
@@ -22,30 +23,26 @@ class MainActivity : Activity(), SensorEventListener {
     private val PERMISSION_REQUEST_CODE = 1
     private lateinit var heartRateTextView: TextView
     private lateinit var runButton: Button
+    private lateinit var targetHeartRateTextView: TextView
+    private lateinit var messageTextView: TextView
     private var age: Int = 0
     private var maxHeartRate: Int = 0
     private var currentPhase: Int = 0
-    private val phases = arrayOf("Warm-up", "Running", "Rest")
-    private val phaseDurations = intArrayOf(30, 120, 30)
-    private val targetHeartRates = arrayOf(0.5, 0.85, 0.65) // 50%, 85%, 65% of max heart rate
-    private lateinit var targetHeartRateTextView: TextView
-    private lateinit var messageTextView: TextView
-    private var heartRate: Int = 0 // Declare heartRate at the class level
-    private val handler = Handler(Looper.getMainLooper()) // Declare handler at the class level
+    private val phases = arrayOf("Warm-up", "Running", "Rest", "Running", "Rest", "Running", "Rest")
+    private val phaseDurations = intArrayOf(10, 10, 10, 10, 10, 10, 10)
+    private val targetHeartRates = arrayOf(0.5, 0.85, 0.65, 0.85, 0.65, 0.85, 0.65)
+    private var heartRate: Int = 0
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        heartRateTextView = findViewById(R.id.heartRateTextView)
-        messageTextView = findViewById(R.id.messageTextView)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        heartRateTextView = findViewById(R.id.heartRateTextView)
-        targetHeartRateTextView = findViewById(R.id.targetHeartRateTextView)
-        runButton = findViewById(R.id.runButton)
+        heartRateTextView = binding.heartRateTextView
+        targetHeartRateTextView = binding.targetHeartRateTextView
+        runButton = binding.runButton
+        messageTextView = binding.messageTextView
 
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
@@ -62,41 +59,33 @@ class MainActivity : Activity(), SensorEventListener {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, start heart rate monitoring
                 sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
             } else {
-                // Permission denied, handle accordingly
+                // Handle permission denied
             }
         }
     }
 
     override fun onSensorChanged(event: SensorEvent) {
         if (event.sensor.type == Sensor.TYPE_HEART_RATE) {
-            val heartRate = event.values[0].toInt()
+            heartRate = event.values[0].toInt()
             heartRateTextView.text = "Heart Rate: $heartRate BPM"
             updateTargetHeartRate(heartRate)
         }
     }
+
     private fun updateTargetHeartRate(heartRate: Int) {
-        // Calculate target heart rate for the current phase
         val targetRate = (maxHeartRate * targetHeartRates[currentPhase]).toInt()
 
-        // Display target heart rate
-        targetHeartRateTextView.text = "Target Heart Rate: $targetRate BPM"
-
-        // Check if the current heart rate is within the target range
+//        targetHeartRateTextView.text = "Target Heart Rate: $targetRate BPM"
+        Log.d("updateTargetHeartRate", "Target Rate: $targetRate BPM")
         if (heartRate < targetRate * 0.9) {
-            // Display a message like "Run faster!"
             messageTextView.text = "Run faster!"
         } else if (heartRate > targetRate * 1.1) {
-            // Display a message like "Slow down!"
             messageTextView.text = "Slow down!"
         } else {
-            // If heart rate is within the target range, clear the message
             messageTextView.text = ""
         }
-
-        // Schedule the next phase...
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -104,54 +93,64 @@ class MainActivity : Activity(), SensorEventListener {
     }
 
     private fun startRunning() {
-        // Get user's age and calculate max heart rate
-        // You should add UI elements and logic to input the age here
-        age = 30 // For example, set age to 30
-        maxHeartRate = calculateMaxHeartRate(age)
 
-        // Start the running phase
+        age = 30 // You should implement age input logic
+        maxHeartRate = calculateMaxHeartRate(age)
         currentPhase = 0
-        runPhase(currentPhase)
+        val targetRate = (maxHeartRate * targetHeartRates[currentPhase]).toInt()
+        targetHeartRateTextView.text = "Target Heart Rate: $targetRate BPM"
+        val curruntTime = System.currentTimeMillis()
+        runPhase(currentPhase, curruntTime)
     }
 
     private fun calculateMaxHeartRate(age: Int): Int {
         return 220 - age
     }
 
-    private fun runPhase(phase: Int) {
+    private fun runPhase(phase: Int, startTimeMillis: Long) {
         if (phase < phases.size) {
-            // Display current phase
-            targetHeartRateTextView.text = "Phase: ${phases[phase]}"
-
-            // Calculate target heart rate for the current phase
             val targetRate = (maxHeartRate * targetHeartRates[phase]).toInt()
+            Log.d("YourTag", "Target Rate: $targetRate BPM")
 
-            // Display target heart rate
-            targetHeartRateTextView.text = "Target Heart Rate: $targetRate BPM"
-
-            // Check if the current heart rate is within the target range
             if (heartRate < targetRate * 0.9) {
-                // Display a message like "Run faster!"
                 messageTextView.text = "Run faster!"
             } else if (heartRate > targetRate * 1.1) {
-                // Display a message like "Slow down!"
                 messageTextView.text = "Slow down!"
             } else {
-                // If heart rate is within the target range, clear the message
                 messageTextView.text = ""
             }
 
-            // Schedule the next phase after the current phase duration
-            val nextPhase = (phase + 1) % phases.size
-            val nextPhaseDuration = phaseDurations[nextPhase] * 1000L // Convert to milliseconds
-            handler.postDelayed({
-                runPhase(nextPhase)
-            }, nextPhaseDuration)
-        } else {
-            // Running is complete, you can add any necessary actions here
-            // For example, display a message or stop monitoring heart rate
-            targetHeartRateTextView.text = "Running Complete"
-            sensorManager.unregisterListener(this)
+            val currentTimeMillis = System.currentTimeMillis()
+            val elapsedTime = currentTimeMillis - startTimeMillis
+            val phaseDurationMillis = phaseDurations[phase] * 1000L
+
+            val currentTimeTextView = findViewById<TextView>(R.id.currentTimeTextView)
+            val elapsedTimeTextView = findViewById<TextView>(R.id.elapsedTimeTextView)
+
+            currentTimeTextView.text = "Current Time: $currentTimeMillis"
+            elapsedTimeTextView.text = "Elapsed Time: $elapsedTime ms"
+
+            if (elapsedTime >= phaseDurationMillis) {
+                // 현재 단계의 시간이 다 지났으므로 다음 단계로 진행
+                val nextPhase = (phase + 1)
+                val targetRate = (maxHeartRate * targetHeartRates[nextPhase]).toInt()
+                Log.d("YourTag", "Next Phase: $nextPhase, Target Rate: $targetRate BPM")
+                targetHeartRateTextView.text = "Target Heart Rate: $targetRate BPM"
+
+                // 다음 페이즈로 넘어갈 때 현재 페이즈가 중복으로 호출되지 않도록 수정
+                if (nextPhase != phase) {
+                    runPhase(nextPhase, System.currentTimeMillis())
+                }
+            } else {
+                // 아직 단계의 시간이 다 지나지 않았으므로 기다립니다.
+                // 예를 들어, 다음 확인을 1초마다 하도록 설정할 수 있습니다.
+                handler.postDelayed({
+                    Log.d("YourTag", "Delayed Phase: $phase")
+                    runPhase(phase, startTimeMillis) // 현재 단계 시작 시간을 그대로 유지
+                }, 1000L)
+            }
         }
     }
+
+
 }
